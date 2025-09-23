7cbf9b57c2b106e3475228b83518e809a179b71c
datnguyen210 <datnguyenftu210@gmail.com>
2025-09-23 11:28:01 +0700

    define constants

--- FtoC.c
+++ FtoC.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0 
+#define UPPER 300 
+#define STEP 20
+
 main()
 {
   float fahr, celsius;
-  float lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
+  fahr = LOWER;
   printf("%s\n", "Fahrenheit to Celsius converter");
-  while(fahr <= upper) {
+  while(fahr <= UPPER) {
     celsius = 5.0 * (fahr - 32.0) / 9.0;
     printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr + STEP;
   }
 }
