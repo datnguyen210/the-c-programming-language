347956b09aef16fc1a849dee8833e2175db80f36
datnguyen210 <datnguyenftu210@gmail.com>
2025-09-26 11:33:05 +0700

    Add function FtoC

--- FtoC.c
+++ FtoC.c
@@ -4,15 +4,23 @@
 #define UPPER 300 
 #define STEP 20
 
-main()
+float FtoC(int fahr);
+
+int main()
 {
   float fahr, celsius;
 
   fahr = LOWER;
   printf("%s\n", "Fahrenheit to Celsius converter");
   while(fahr <= UPPER) {
-    celsius = 5.0 * (fahr - 32.0) / 9.0;
-    printf("%3.0f %6.1f\n", fahr, celsius);
+    printf("%3.0f %6.1f\n", fahr, FtoC(fahr));
     fahr = fahr + STEP;
   }
+  return 0;
+}
+
+float FtoC(int fahr) {
+  float cel;
+  cel = 5.0 * (fahr - 32.0) / 9.0;
+  return cel;
 }
